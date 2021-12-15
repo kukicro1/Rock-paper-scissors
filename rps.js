854399a8bc3f359d34395091155aba9a85278b45
kukicro1 <krunomraz@gmail.com>
2021-12-15 19:24:59 +0100

    first branch commit

--- rps.js
+++ rps.js
@@ -1,118 +1,118 @@
-function computerPlay(){
-    let rps = "";
-    let randomValue = Math.random();
+let user = 0;
+let computer = 0;
+let rps = "";    
+let whoWon = "";
+let result = "";
+let playerSelection;
+const buttons = document.querySelectorAll('.player');
+const display = document.querySelector('#result');
+const replay = document.querySelector('#replay');
 
+display.style.width = 'auto';
+display.style.height = '150px';
+display.setAttribute('style', 'white-space: pre;');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.value;
+        game();
+    }) 
+});
+
+
+function game() {
+        playRound()
+        if (user === 5){
+            display.textContent = "You beat the machine. Gratz!!";
+            user = 0;
+            computer = 0;
+        }
+        else if (computer === 5){
+            display.textContent = "Machine beat your. Loser!!";
+            user = 0;
+            computer = 0;
+        }
+}
+
+replay.addEventListener('click', () => {
+    replayGame();
+})
+
+function replayGame() {
+    game();
+}
+
+function computerPlay(){
+    let randomValue = Math.random();
     if (randomValue < 0.333) {
         rps = "rock";
         return rps;
-    }
-    
+    } 
     else if (0.334 < randomValue && randomValue < 0.666) {
         rps = "paper";
         return rps;
     }
-
     else {
         rps = "scissors";
         return rps;
     } 
 }
-let user = 0;
-let computer = 0;
 
 function playRound() {
-    let whoWon = "";
-    let result = "";
-    let playerSelection = prompt().toLowerCase();
     let computerSelection = computerPlay();
-
     if (playerSelection == computerSelection) {
         whoWon = "It's a tie.";
-        console.log(whoWon);
         user = user;
         computer = computer;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
         whoWon = "Machine beat you. Loser. Rock is beaten by paper.";
-        console.log(whoWon);
         computer += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-
     else if (playerSelection == "scissors" && computerSelection == "rock") {
         whoWon = "Machine beat you. Loser. Scissors are beaten by rock.";
-        console.log(whoWon);
         computer += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-
     else if (playerSelection == "paper" && computerSelection == "scissors") {
         whoWon = "Machine beat you. Loser. Paper is beaten by scissors.";
-        console.log(whoWon);
         computer += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-
     else if (playerSelection == "paper" && computerSelection == "rock") {
         whoWon = "You beat the machine. Paper beats rock.";
-        console.log(whoWon);
         user += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-
     else if (playerSelection == "rock" && computerSelection == "scissors") {
         whoWon = "You beat the machine. Rock beats scissors.";
-        console.log(whoWon);
         user += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-    
     else if (playerSelection == "scissors" && computerSelection == "paper") {
         whoWon = "You beat the machine. Scissors beat paper.";
-        console.log(whoWon);
         user += 1;
         result = `You: ${user} \nMachine: ${computer}`;
-        console.log(result);
+        display.textContent = `${whoWon} \n${result}`;
         return;
     }
-
     else {
         whoWon = "Please enter: paper, rock, or scissors."
-        console.log(whoWon);
-        return
+        display.textContent = `${whoWon}`;
+        return;
     }
 }
-
-
-
-function game() {
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-if (user > computer){
-        console.log("You beat the machine. Gratz!!")
-    }
-    else if (user < computer){
-        console.log("Machine beat your. Loser!!")
-    }
-    else {
-        console.log("It's a tie.")
-    }
-}
-game();
-alert("ovo je kraj")
